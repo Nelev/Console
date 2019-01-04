800020bd41330e3929d460acb22a55f67ac48573
nlvz <smacchia.mattia@gmail.com>
2019-01-04 15:34:58 +0100

    chore: first app setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Root from "./views/Root";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import "./index.css";
+
+const App = (
+    <IntlProvider locale="en">
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Root} />
+            </Switch>
+        </BrowserRouter>
+    </IntlProvider>
+);
+
+ReactDOM.render(App, document.getElementById("root"));
---
+++ src/views/Root/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Root extends React.Component {
+    render() {
+        return <div>ciao</div>;
+    }
+}
+
+export default Root;
