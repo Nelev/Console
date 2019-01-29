eaf9cea1b694f6a9151e0896486c809958e72d92
Nelev <smacchia.mattia@gmail.com>
2019-01-29 21:35:23 +0100

    chore: README file

--- src/views/DashBoard/index.jsx
+++ src/views/DashBoard/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { VictoryPie } from "victory";
+import { VictoryArea, VictoryChart, VictoryTheme } from "victory";
 
 import "./style.less";
 
@@ -7,7 +7,18 @@ class DashBoard extends React.Component {
     render() {
         return (
             <div className="v-DashBoard">
-                <VictoryPie />
+                <VictoryChart theme={VictoryTheme.material}>
+                    <VictoryArea
+                        style={{ data: { fill: "#c43a31" } }}
+                        data={[
+                            { x: 1, y: 2, y0: 0 },
+                            { x: 2, y: 3, y0: 1 },
+                            { x: 3, y: 5, y0: 1 },
+                            { x: 4, y: 4, y0: 2 },
+                            { x: 5, y: 6, y0: 2 }
+                        ]}
+                    />
+                </VictoryChart>
             </div>
         );
     }
